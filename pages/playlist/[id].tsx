eba9e29317ee2f058f787e80fcf173481c102cf5
tahmdev <business@tomschuelke.dev>
2023-01-17 09:05:59 +0100

    Implement player

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,6 +1,7 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { Player } from "../../components/Player/Player";
 import { IVideo } from "../../interfaces/IVideo";
 import styles from "../styles/playlists.module.css";
 
@@ -10,33 +11,19 @@ const Playlists: NextPage = () => {
 
   async function addVideos(id: string) {
     const response = await fetch(`../api/videos/${id}`);
-    console.log(response);
-
     if (!response.ok) throw new Error();
     const videos: IVideo[] = await response.json();
-    console.log(videos);
-
     setVideos(videos);
   }
 
   useEffect(() => {
     if (typeof router.query.id !== "string") return;
-    console.log(router.query.id);
-
     addVideos(router.query.id);
   }, [router.query]);
 
   if (!videos?.length) return <div>LOADING...</div>;
 
-  return (
-    <div>
-      <ul>
-        {videos.map((e, i) => {
-          return <li key={i}>{e.snippet.title}</li>;
-        })}
-      </ul>
-    </div>
-  );
+  return <Player videoProps={videos} title />;
 };
 
 export default Playlists;
