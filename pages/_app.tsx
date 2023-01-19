dcdffcafcf41604f9d00e7baea305457ca6a772d
tahmdev <business@tomschuelke.dev>
2023-01-19 08:15:28 +0100

    Implement ToastDisplay

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ToastDisplay } from "../components/Toast/Toast";
+import { createContext, Dispatch, useReducer } from "react";
+import { ToastActions, toastReducer } from "../components/Toast/ToastReducer";
 
+interface IToastContext {
+  dispatch: Dispatch<ToastActions>;
+}
+
+export const ToastContext = createContext<IToastContext>({} as IToastContext);
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [toastNotifications, toastDispatch] = useReducer(toastReducer, []);
+  return (
+    <>
+      <ToastContext.Provider value={{ dispatch: toastDispatch }}>
+        <ToastDisplay
+          dispatch={toastDispatch}
+          notifications={toastNotifications}
+          autoRemove={5000}
+        />
+        <Component {...pageProps} />
+      </ToastContext.Provider>
+    </>
+  );
 }
