2eca7c26248a72987a64cc09aa84a9ab01a46e15
tahmdev <business@tomschuelke.dev>
2023-01-30 12:07:24 +0100

    Style playlistCard

--- components/PlaylistCard.tsx
+++ components/PlaylistCard.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction, useState } from "react";
 import IPlaylist from "../interfaces/IPlaylist";
 
 interface Props {
@@ -11,33 +11,45 @@ export const PlaylistCard: React.FC<Props> = ({
   removePlaylist,
   setSelectedPlaylists,
 }) => {
+  const [checked, setChecked] = useState(false);
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.checked) {
+      setChecked(true);
       setSelectedPlaylists((prev) => [...prev, playlist.id]);
     } else {
+      setChecked(false);
       setSelectedPlaylists((prev) => prev.filter((el) => el !== playlist.id));
     }
   };
   return (
-    <div className="flex">
+    <div className={`flex ${checked ? "bg-slate-700" : "bg-slate-500"}`}>
       <input
+        className="hidden"
         type="checkbox"
         name="playlist"
         id={playlist.id}
         onChange={onChange}
+        checked={checked}
       />
-      <label htmlFor={playlist.id} className="flex">
+      <label htmlFor={playlist.id} className="flex h-24 w-full cursor-pointer">
         <img
+          className="h-full"
           src={playlist.snippet.thumbnails.medium.url}
           alt={`Thumbnail of playlist ${playlist.snippet.title} `}
         />
-        <div>
+        <div className="flex flex-col justify-center">
           <span>{playlist.snippet.title} </span>
           <span> {playlist.snippet.channelTitle} </span>
           <span>Length: {playlist.contentDetails.itemCount} </span>
         </div>
       </label>
-      <button onClick={() => removePlaylist(playlist.id)}>Remove</button>
+      <button
+        aria-label={`Remove playlist ${playlist.snippet.title}`}
+        className="my-auto ml-auto mr-2 aspect-square rounded bg-red-500 py-1 px-3 font-bold"
+        onClick={() => removePlaylist(playlist.id)}
+      >
+        🗑️
+      </button>
     </div>
   );
 };
