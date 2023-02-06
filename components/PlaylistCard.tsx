fa8464c87f4f9754cecba43721aa4dfa1e7693df
tahmdev <business@tomschuelke.dev>
2023-02-06 10:11:48 +0100

    Fix playlistcard style
    Properly handle long titles.
    Only active hover state when hovering a clickable area

--- components/PlaylistCard.tsx
+++ components/PlaylistCard.tsx
@@ -23,37 +23,44 @@ export const PlaylistCard: React.FC<Props> = ({
   };
   return (
     <div
-      className={`flex ${
-        checked ? "bg-slate-700" : "bg-slate-500 hover:bg-slate-600"
-      }`}
+      className={` flex w-full justify-between ${
+        checked ? "[&>*]:bg-slate-700" : "[&>*]:bg-slate-500"
+      } overflow-hidden`}
     >
-      <input
-        className="hidden"
-        type="checkbox"
-        name="playlist"
-        id={playlist.id}
-        onChange={onChange}
-        checked={checked}
-      />
-      <label htmlFor={playlist.id} className="flex h-24 w-full cursor-pointer">
-        <img
-          className="h-full"
-          src={playlist.snippet.thumbnails.medium.url}
-          alt={`Thumbnail of playlist ${playlist.snippet.title} `}
+      <div className="peer flex-1 overflow-hidden hover:bg-slate-600 ">
+        <input
+          className="hidden"
+          type="checkbox"
+          name="playlist"
+          id={playlist.id}
+          onChange={onChange}
+          checked={checked}
         />
-        <div className="flex flex-col justify-center">
-          <span>{playlist.snippet.title} </span>
-          <span> {playlist.snippet.channelTitle} </span>
-          <span>Length: {playlist.contentDetails.itemCount} </span>
-        </div>
-      </label>
-      <button
-        aria-label={`Remove playlist ${playlist.snippet.title}`}
-        className="my-auto ml-auto mr-2 aspect-square rounded bg-red-500 py-1 px-3 font-bold"
-        onClick={() => removePlaylist(playlist.id)}
-      >
-        🗑️
-      </button>
+        <label
+          htmlFor={playlist.id}
+          className="flex h-24 w-full cursor-pointer "
+        >
+          <img
+            className="mr-1 h-full"
+            src={playlist.snippet.thumbnails.medium.url}
+            alt={`Thumbnail of playlist ${playlist.snippet.title} `}
+          />
+          <div className="flex w-full flex-col overflow-hidden [&>*]:overflow-hidden [&>*]:text-ellipsis [&>*]:whitespace-nowrap">
+            <p>{playlist.snippet.title} </p>
+            <p> {playlist.snippet.channelTitle} </p>
+            <p>Length: {playlist.contentDetails.itemCount} </p>
+          </div>
+        </label>
+      </div>
+      <div className="flex align-middle peer-hover:bg-slate-600">
+        <button
+          aria-label={`Remove playlist ${playlist.snippet.title}`}
+          className="my-auto ml-auto mr-2 aspect-square rounded bg-red-500 py-1 px-3 font-bold hover:bg-red-800"
+          onClick={() => removePlaylist(playlist.id)}
+        >
+          🗑️
+        </button>
+      </div>
     </div>
   );
 };
